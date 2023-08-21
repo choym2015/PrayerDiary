a024f776c883833f13a32240ad7aafbc31cf951b
Youngmin Cho <choym2015@gmail.com>
2023-08-21 17:24:10 -0400

    Initial project config

---
+++ PrayerDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrayerDiary
+//
+//  Created by Youngmin Cho on 8/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
